266ca1ad4270e5dd28a35cf87de268ade4d3f9dd
Josías Rodríguez <rodriguezjosias@gmail.com>
2026-08-11 13:16:22 -0400

    style: establish global design system

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
+import "./app/styles/globals.css"
+import "./app/styles/reset.css"
+import "./app/styles/tokens.css"
+import "./app/styles/typography.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
