a9f3f43c287ec1140cadf2de4e4dc7f3d5e62d0d
Josías Rodríguez <rodriguezjosias@gmail.com>
2026-08-11 12:48:02 -0400

    chore: initialize Vite React TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  const name: string = "Josías";
+  const age: number = 20;
+
+  return (
+    <>
+      <h1>
+        hello {name}, tienes {age} años. 
+      </h1>
+    </>
+  )
+}
+
+export default App
